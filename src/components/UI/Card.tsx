db3dcf5b4f01139ed72805787578120949319d47
Ukana.Bastreet <s.ppp@bk.ru>
2025-08-31 02:54:20 +0300

    add styles to Card comp

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -2,9 +2,11 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px 0px;
-  border-radius: 12px;
+  box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px 0px;
+  // box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px 0px;
+  border-radius: 10px;
   padding: 16px 20px;
+  
 `;
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
